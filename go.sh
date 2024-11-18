16c258c30c5d6b9d005573e48e35219cbb3f7e5a
nucleargezi <604223110@qq.com>
2024-11-18 20:08:59 +0800

    提交信息

--- go.sh
+++ go.sh
@@ -1,4 +1,4 @@
-git add .                 # 添加所有更改
-git commit -m "提交信息"   # 提交更改
-git pull origin master       # 拉取远程更改（确保同步）
-git push origin master       # 推送到远程仓库
+git add .
+git commit -m "提交信息"
+git pull origin master
+git push origin master
