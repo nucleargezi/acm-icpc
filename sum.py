45a30c03f8ded741381b91084e51f0d4660cbf17
nucleargezi <604223110@qq.com>
2024-12-31 17:46:46 +0800

    提交信息

---
+++ sum.py
@@ -0,0 +1,12 @@
+import os
+
+def count_cpp_files(directory):
+    cpp_count = 0
+    for root, _, files in os.walk(directory):
+        cpp_count += sum(1 for file in files if file.endswith('.cpp'))
+    return cpp_count
+
+if __name__ == "__main__":
+    current_directory = os.getcwd()  # 获取当前文件夹路径
+    cpp_file_count = count_cpp_files(current_directory)
+    print(f"cnt = {cpp_file_count}")
