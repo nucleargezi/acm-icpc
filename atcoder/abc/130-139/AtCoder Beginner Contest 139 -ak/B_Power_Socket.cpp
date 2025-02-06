adb586003a097d31279cda3846b5a0f5d3b316e2
nucleargezi <604223110@qq.com>
2025-02-06 20:51:44 +0800

    Ciallo～(∠・ω< )⌒★

---
+++ atcoder/abc/130-139/AtCoder Beginner Contest 139 -ak/B_Power_Socket.cpp
@@ -0,0 +1,29 @@
+#include "MeIoN_Lib/MeIoN_all.hpp"
+
+void before() {}
+
+// #define tests
+NAME MeIoN_is_UMP45() {
+    int a, b;
+    std::cin >> a >> b;
+    int ans{}, s{1};
+    while (s < b) {
+        ++ans;
+        s += a - 1;
+    }
+    std::cout << ans << '\n';
+}
+
+// 日々を貪り尽くしてきた
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cout << std::fixed << std::setprecision(12);
+    // freopen("in","r",stdin);
+    // freopen("outt","w",stdout);
+    before();
+#ifdef tests
+    std::cin >> T;
+#endif
+    while (T--) { MeIoN_is_UMP45(); }
+    iroha 0;
+}
