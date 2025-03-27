1aaedfba8657f327e33cdeae70980648f75ec7b8
nucleargezi <604223110@qq.com>
2025-03-27 13:32:07 +0800

    Ciallo～(∠・ω< )⌒★

---
+++ Prov/2019/陕西/104460L.cpp
@@ -0,0 +1,29 @@
+#include "MeIoN_Lib/MeIoN_all.hpp"
+
+void before() {}
+
+#define tests
+NAME MeIoN_is_UMP45() {
+    LL(l, r);
+    ll ans{1};
+    FOR(i, l, r + 1) {
+        vector v = s_to_vec(std::to_string(i), '0');
+        for (ll x : v) ans *= x;
+        if (not ans) break;
+    }
+    UL(ans);
+}
+
+// 日々を貪り尽くしてきた
+int main() {
+    std::cin.tie(nullptr)->sync_with_stdio(false);
+    std::cout << std::fixed << std::setprecision(12);
+    // freopen("in","r",stdin);
+    // freopen("outt","w",stdout);
+    before();
+#ifdef tests
+    INT(t); FOR(t)
+#endif
+    MeIoN_is_UMP45();
+    iroha 0;
+}
