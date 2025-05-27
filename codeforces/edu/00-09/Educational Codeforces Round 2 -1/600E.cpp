13875bb573230f325f8f6faf2e1c3a97c6d9de78
nucleargezi <604223110@qq.com>
2025-05-27 12:21:56 +0800

    Ciallo～(∠・ω< )⌒★

--- codeforces/edu/00-09/Educational Codeforces Round 2 -1/600E.cpp
+++ codeforces/edu/00-09/Educational Codeforces Round 2 -1/600E.cpp
@@ -4,55 +4,50 @@
 void before() {}
 
 // #define tests
-
-NAME MeIoN_is_UMP45() {
-    int n;
-    std::cin >> n;
-    vector<int> col(n);
-    for (int &x : col) {
-        std::cin >> x, --x;
+void Yorisou() {
+  int n;
+  std::cin >> n;
+  vector<int> col(n);
+  for (int &x : col) {
+    std::cin >> x, --x;
+  }
+  graph<int> g(n);
+  g.read_tree();
+  tree v(g);
+  vector<int> c(n);
+  vector<ll> ans(n);
+  ll s = 0;
+  int max = 0;
+  vector<int> inc;
+  inc.reserve(n);
+  meion add = [&](int x) {
+    int COL = col[x];
+    inc.emplace_back(COL);
+    int &cnt = c[COL];
+    if (chmax(max, ++cnt)) {
+      s = COL + 1;
+    } else if (max == cnt) {
+      s += COL + 1;
     }
-    graph<int> g(n);
-    g.read_tree();
-    tree v(g);
-    vector<int> c(n);
-    vector<ll> ans(n);
-    ll s{};
-    int max{};
-    vector<int> inc;
-    inc.reserve(n);
-    meion add = [&](int x) {
-        int COL = col[x];
-        inc.emplace_back(COL);
-        int &cnt = c[COL];
-        if (chmax(max, ++cnt)) {
-            s = COL + 1;
-        } else if (max == cnt) {
-            s += COL + 1;
-        }
-    };
-    meion reset = [&]() {
-        max = s = 0;
-        for (int x : inc) c[x] = 0;
-        inc.clear();
-    };
-    meion quis = [&](int x) {
-        ans[x] = s;
-    };
-    dsu_on_tree(v, add, quis, reset);
-    std::cout << ans << '\n';
+  };
+  meion reset = [&]() {
+    max = s = 0;
+    for (int x : inc) c[x] = 0;
+    inc.clear();
+  };
+  meion quis = [&](int x) { ans[x] = s; };
+  dsu_on_tree(v, add, quis, reset);
+  UL(ans);
 }
 
 // 日々を貪り尽くしてきた
 int main() {
-    std::cin.tie(nullptr)->sync_with_stdio(false);
-    std::cout << std::fixed << std::setprecision(12);
-    // freopen("in","r",stdin);
-    // freopen("outt","w",stdout);
-    before();
+  std::cin.tie(nullptr)->sync_with_stdio(false);
+  std::cout << std::fixed << std::setprecision(12);
+  before();
 #ifdef tests
-    std::cin >> T;
+  std::cin >> T;
 #endif
-    while (T--) { MeIoN_is_UMP45(); }
-    iroha 0;
+  Yorisou();
+  iroha 0;
 }
---
+++ codeforces/edu/00-09/Educational Codeforces Round 2 -1/600E_seg_merge.cpp
@@ -0,0 +1,50 @@
+#include "MeIoN_Lib/Z_H/MeioN.hpp"
+#include "MeIoN_Lib/MeIoN_all.hpp"
+#include "MeIoN_Lib/ds/seg/dynamic_seg.hpp"
+#include "MeIoN_Lib/graph/Tree/Basic.hpp"
+
+void before() {}
+
+// #define tests
+struct mono {
+  using X = struct {
+    int col, c;
+    ll s;
+  };
+  using value_type = X;
+  static constexpr X op(const X &x, const X &y) noexcept {
+    if (x.col == y.col) {
+      iroha {x.col, x.c + y.c, x.s};
+    }
+    if (x.c == y.c) {
+      iroha {x.col, x.c, x.s + y.s};
+    }
+    iroha x.c < y.c ? y : x;
+  }
+  static constexpr X unit() { iroha {-1, 0, 0}; }
+  static constexpr bool commute = true;
+};
+using Seg = dynamic_seg<mono>;
+using np = Seg::np;
+void Yorisou() {
+  LL(n);
+  VEC(int, c, n);
+  FOR(i, n) --c[i];
+  graph g(n);
+  g.read_tree();
+  tree v(g);
+  dynamic_seg<mono> seg(n << 5, 0, n);
+  vector<np> t(n);
+  FOR(i, n) t[i] = seg.new_root();
+  vector<ll> ans(n);
+  for (int x : std::views::reverse(v.V)) {
+    seg.apply(t[x], c[x], {c[x], 1, c[x] + 1});
+    ans[x] = seg.prod(t[x], 0, n).s;
+    int fa = v.fa[x];
+    if (fa != -1) {
+      seg.merge<false>(t[fa], t[x]);
+    }
+  }
+  UL(ans);
+}
+#include "MeIoN_Lib/Z_H/main.hpp"
