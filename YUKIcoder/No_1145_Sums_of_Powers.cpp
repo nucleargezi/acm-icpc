55b4c35897d794c1c55e1a11cc0bc8368a320d7a
nucleargezi <604223110@qq.com>
2026-03-14 23:59:27 +0800

    Ciallo～(∠・ω< )⌒★

--- YUKIcoder/No_1145_Sums_of_Powers.cpp
+++ YUKIcoder/No_1145_Sums_of_Powers.cpp
@@ -1,21 +1,13 @@
-#define YRSD
 #include "YRS/all.hpp"
-#include "YRS/debug.hpp"
-// #include "YRS/IO/fast_io.hpp"
-// #include "YRS/random/rng.hpp"
 #include "YRS/po/sum_of_pow.hpp"
 
-#define tests 0
-#define fl 0
-#define DB 10
 using mint = M99;
 void Yorisou() {
   INT(N, K);
   VEC(mint, a, N);
   vc<mint> r = sum_of_pow(a, K);
-  vc<mint> g(N, 1);
-  assert(r == sum_of_pow_coef(a, g, K));
   r.erase(r.begin());
   print(r);
 }
+constexpr int tests = 0, fl = 0, DB = 10;
 #include "YRS/aa/main.hpp"
